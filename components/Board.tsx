d9558a2d55f819df00912ebc31e3fdbe72c3e1ee
Owenn Gimli <75826315+owenn2106@users.noreply.github.com>
2023-06-18 14:28:34 +0700

    fix error when moving board

--- components/Board.tsx
+++ components/Board.tsx
@@ -31,60 +31,61 @@ const Board = () => {
       entries.splice(destination.index, 0, removed);
       const rearrangedColumns = new Map(entries);
       setBoardState({ ...board, columns: rearrangedColumns });
-    }
-
-    // Handle card drag
-    const columns = Array.from(board.columns);
-    const startColIndex = columns[Number(source.droppableId)];
-    const finishColIndex = columns[Number(destination.droppableId)];
-
-    const startCol: Column = {
-      id: startColIndex[0],
-      todos: startColIndex[1].todos,
-    };
-
-    const finishCol: Column = {
-      id: finishColIndex[0],
-      todos: finishColIndex[1].todos,
-    };
-
-    if (!startCol || !finishCol) return;
-    if (source.index === destination.index && startCol === finishCol) return;
-
-    const newTodos = startCol.todos;
-    const [todoMoved] = newTodos.splice(source.index, 1);
-
-    if (startCol.id === finishCol.id) {
-      // SAME COLUMN CARD DRAG
-      newTodos.splice(destination.index, 0, todoMoved);
-      const newCol = {
-        id: startCol.id,
-        todos: newTodos,
-      };
-      const newColumns = new Map(board.columns);
-      newColumns.set(startCol.id, newCol);
-
-      setBoardState({ ...board, columns: newColumns });
     } else {
-      // DRAGGING TO ANOTHER COLUMN
-      const finishTodos = Array.from(finishCol.todos);
-      finishTodos.splice(destination.index, 0, todoMoved);
+      // Handle card drag
+      const columns = Array.from(board.columns);
+      const startColIndex = columns[Number(source.droppableId)];
+      const finishColIndex = columns[Number(destination.droppableId)];
+      console.log(source, destination);
 
-      const newColumns = new Map(board.columns);
-      const newCol = {
-        id: startCol.id,
-        todos: newTodos,
+      const startCol: Column = {
+        id: startColIndex[0],
+        todos: startColIndex[1].todos,
       };
 
-      newColumns.set(startCol.id, newCol);
-      newColumns.set(finishCol.id, {
-        id: finishCol.id,
-        todos: finishTodos,
-      });
+      const finishCol: Column = {
+        id: finishColIndex[0],
+        todos: finishColIndex[1].todos,
+      };
 
-      updateTodoInDB(todoMoved, finishCol.id);
+      if (!startCol || !finishCol) return;
+      if (source.index === destination.index && startCol === finishCol) return;
 
-      setBoardState({ ...board, columns: newColumns });
+      const newTodos = startCol.todos;
+      const [todoMoved] = newTodos.splice(source.index, 1);
+
+      if (startCol.id === finishCol.id) {
+        // SAME COLUMN CARD DRAG
+        newTodos.splice(destination.index, 0, todoMoved);
+        const newCol = {
+          id: startCol.id,
+          todos: newTodos,
+        };
+        const newColumns = new Map(board.columns);
+        newColumns.set(startCol.id, newCol);
+
+        setBoardState({ ...board, columns: newColumns });
+      } else {
+        // DRAGGING TO ANOTHER COLUMN
+        const finishTodos = Array.from(finishCol.todos);
+        finishTodos.splice(destination.index, 0, todoMoved);
+
+        const newColumns = new Map(board.columns);
+        const newCol = {
+          id: startCol.id,
+          todos: newTodos,
+        };
+
+        newColumns.set(startCol.id, newCol);
+        newColumns.set(finishCol.id, {
+          id: finishCol.id,
+          todos: finishTodos,
+        });
+
+        updateTodoInDB(todoMoved, finishCol.id);
+
+        setBoardState({ ...board, columns: newColumns });
+      }
     }
   };
 
